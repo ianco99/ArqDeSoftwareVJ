79d3f318131d7104372c48e3fac942947421254b
ianco99 <71154684+ianco99@users.noreply.github.com>
2026-01-19 18:57:51 -0300

    feat: Create entity class

---
+++ ArqVJ2026/Assets/Code/Architecture/GameLogic/Entities/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace ZooArchitect.Architecture.Entities
+{
+    public abstract class Entity
+    {
+        public const uint UNASSIGNED_ENTITY_ID = 0;
+
+        public uint ID;
+        private Coordinate coordinate;
+
+        public Entity(uint ID, Coordinate coordinate)
+        {
+            this.ID = ID;
+            this.coordinate = coordinate;
+        }
+    }
+}
---
+++ ArqVJ2026/Assets/Code/Architecture/GameLogic/Math/Coordinate.cs
@@ -0,0 +1,8 @@
+﻿namespace ZooArchitect.Architecture.Entities
+{
+    public struct Coordinate
+    {
+        public int x;
+        public int y;
+    }
+}
