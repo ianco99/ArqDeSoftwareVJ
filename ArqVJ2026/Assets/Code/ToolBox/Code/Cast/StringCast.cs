707d4d9175389dd4fd5ed077f6eb615c31837c9d
ianco99 <71154684+ianco99@users.noreply.github.com>
2026-01-20 20:40:21 -0300

    chore: Add NPOI dependencies

---
+++ ArqVJ2026/Assets/Code/ToolBox/Code/Cast/StringCast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ianco99.ToolBox.Cast
+{
+    public static class StringCast
+    {
+        public static object Convert(string value, Type targetType)
+        {
+            //if (value == string)
+            //    return;
+        }
+    }
+}
