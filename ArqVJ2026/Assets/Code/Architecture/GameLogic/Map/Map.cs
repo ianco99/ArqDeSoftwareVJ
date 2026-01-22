dc0b5fc91d5547e2e3333b76ace5fb0b130883f2
ianco99 <71154684+ianco99@users.noreply.github.com>
2026-01-22 20:11:27 -0300

    feat: Create map class

---
+++ ArqVJ2026/Assets/Code/Architecture/GameLogic/Map/Map.cs
@@ -0,0 +1,38 @@
+﻿using ianco99.ToolBox.Blueprints;
+using System.Collections.Generic;
+
+namespace ZooArchitect.Architecture.GameLogic
+{
+    public sealed class Map
+    {
+        private uint sizeX;
+        private uint sizeY;
+
+        private Tile[,] tiles;
+
+        private Dictionary<int, TileData> tileDatas;
+
+        public Map()
+        {
+            tileDatas = new Dictionary<int, TileData>();
+
+        }
+    }
+
+    
+
+    public struct Tile
+    {
+         public int tileType;
+    }
+
+    public struct TileData
+    {
+        [BlueprintParameter("TileType")] public bool walkable;
+        [BlueprintParameter("TileType")] public bool isStructure;
+        [BlueprintParameter("TileType")] public bool isAnimalContainer;
+        [BlueprintParameter("TileType")] public bool canSpawnHumans;
+        [BlueprintParameter("TileType")] public bool canDispawnHumans;
+    }
+
+}
